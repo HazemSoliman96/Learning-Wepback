283fe5beedca76907090198c1ce372e319019d5f
Hazem Soliman <hazemsoliman015@gmail.com>
2021-05-13 02:22:51 +0200

    asset management [css]

--- webpack-config.js
+++ webpack-config.js
@@ -1,9 +1,54 @@
-const path = require('path');
+const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader, 
+          {
+            loader: "css-loader"
+          },
+          {
+            loader:"resolve-url-loader"
+          },
+          {
+            loader:"sass-loader",
+            options: {
+              sourceMap: true,
+            }
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin(),
+    ],
   },
 };
